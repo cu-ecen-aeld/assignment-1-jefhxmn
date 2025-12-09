0ec38aa55d21d9680e2997605bd4e70329dc5924
jefhxmn <jeferson.oliveira@gmail.com>
2025-12-09 20:00:23 -0300

    Implementing writer sh

---
+++ finder-app/writer.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+if [ $# -ne 2 ]; then 
+    echo "Error: Two parameters required."
+    echo "Usage: $0 <writefile> <writestr>"
+    exit 1
+fi 
+
+writefile=$1
+writestr=$2
+
+writedir=$(dirname "$writefile")
+
+mkdir -p "$writedir"
+if [ $? -ne 0 ]; then 
+    echo "Error: Could not create directory path '$writedir'"
+    exit 1
+fi
+
+echo "$writestr" > "$writefile"
+if [ $? -ne 0 ]; then 
+    echo "Error: Could not create or write file '$writefile'"
+    exit 1
+fi
